921e4c074b5239bcc95d40a141d4add7035de075
Alister <graham.alister@gmail.com>
2022-08-24 13:26:40 -0500

    created basic button

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+document.getElementById('clickme').onclick = partyPurple
+
+function partyPurple() {
+    document.querySelector('h1').style.backgroundColor = 'rgba(241,63,247,1)'
+    document.querySelector('h1').style.color = 'white'
+  }
